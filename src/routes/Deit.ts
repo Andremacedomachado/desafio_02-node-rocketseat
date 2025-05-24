63c163b9b48434cb1053fae876f4b153dcdd3e14
André Macedo Machado <37791756+Andremacedomachado@users.noreply.github.com>
2025-05-24 17:57:48 -0300

    Add update diet.

--- src/routes/Deit.ts
+++ src/routes/Deit.ts
@@ -46,4 +46,54 @@ export function deitRoutes(app: FastifyInstance) {
 
     return reply.status(201).send();
   });
+
+  app.put("/:id", async (req, reply) => {
+    const PutDietRequestBody = z.object({
+      name: z.string().optional(),
+      description: z.string().optional(),
+      type: z.boolean().optional(),
+      date_diet: z.coerce.date().optional(),
+    });
+
+    type UpdateDietData = z.infer<typeof PutDietRequestBody>;
+
+    const PutDietRequestParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { name, type, date_diet, description } = PutDietRequestBody.parse(
+      req.body
+    );
+    const { id } = PutDietRequestParams.parse(req.params);
+
+    const [dietExists] = await knex("diets").select("*").where({
+      id,
+    });
+
+    if (!dietExists) {
+      return reply.status(400).send({ message: "Diet is not exists" });
+    }
+
+    const dataToUpdate = {
+      name,
+      description,
+      type,
+      date_diet: date_diet ? date_diet.toISOString() : undefined,
+    };
+
+    Object.entries(dataToUpdate).forEach(([key, value]) => {
+      if (typeof value === "undefined") {
+        delete dataToUpdate[key as keyof UpdateDietData];
+      }
+    });
+
+    await knex("diets")
+      .update({
+        ...dataToUpdate,
+      })
+      .where({
+        id,
+      });
+    return reply.status(204).send();
+  });
 }
